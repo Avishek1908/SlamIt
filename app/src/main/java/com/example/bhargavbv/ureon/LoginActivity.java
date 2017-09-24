6f414b6a87f0aa719c97ae06111a165249b8db36
BhargavBV <bhargavauden1998@gmail.com>
2017-09-24 21:39:58 +0530

    Login Backend

--- app/src/main/java/com/example/bhargavbv/ureon/LoginActivity.java
+++ app/src/main/java/com/example/bhargavbv/ureon/LoginActivity.java
@@ -1,13 +1,43 @@
 package com.example.bhargavbv.ureon;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import static android.R.attr.name;
 
 public class LoginActivity extends AppCompatActivity {
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
+    private SignInButton signInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signInButton = (SignInButton)findViewById(R.id.sign_in_button);
+
+
+
     }
+
 }
