c8ea688cefe2e6e3dc4c9aa813fef2a097c00a26
Avishek <avishekm19@gmail.com>
2017-10-09 22:47:20 +0530

    lol

---
+++ app/src/main/java/com/example/bhargavbv/ureon/UserProfile/VideoActivity.java
@@ -0,0 +1,15 @@
+package com.example.bhargavbv.ureon.UserProfile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bhargavbv.ureon.R;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
