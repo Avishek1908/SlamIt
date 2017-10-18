882e78888104bef951c8322a6ee6d2c3f72ec751
Avishek <avishekm19@gmail.com>
2017-10-19 01:16:05 +0530

    #Done

---
+++ app/src/main/java/com/example/bhargavbv/ureon/Challenges/ChallengeActivity.java
@@ -0,0 +1,15 @@
+package com.example.bhargavbv.ureon.Challenges;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bhargavbv.ureon.R;
+
+public class ChallengeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge);
+    }
+}
