9121bbd325d4271ab9c2f396f57a234ab16aa2ee
Avishek <avishekm19@gmail.com>
2017-10-20 01:47:54 +0530

    Connect to your phone and see if it works

--- app/src/main/java/com/example/bhargavbv/ureon/UploadVideo/CaptionActivity.java
+++ app/src/main/java/com/example/bhargavbv/ureon/UploadVideo/CaptionActivity.java
@@ -1,19 +1,29 @@
 package com.example.bhargavbv.ureon.UploadVideo;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.ThumbnailUtils;
+import android.media.audiofx.BassBoost;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -31,13 +41,18 @@ import com.google.firebase.storage.UploadTask;
 import java.io.ByteArrayOutputStream;
 
 public class CaptionActivity extends AppCompatActivity {
+    private ImageButton imageButton;
+    private EditText editText;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
 
     DatabaseReference dref;
     StorageReference sref;
     FirebaseUser user;
     private FirebaseAuth mAuth;
     Uri videoUri;
-    public String filepath,UID;
+    public String filepath, UID;
     private String pushkey;
     Bitmap bmThumbnail;
 
@@ -46,11 +61,71 @@ public class CaptionActivity extends AppCompatActivity {
 
     Boolean cap = false;
 
-    private static final String TAG ="CaptionsActivity";
+    private static final String TAG = "CaptionsActivity";
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if(grantResults.length>10&&grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                    configureImageButton();
+                return;
+
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caption);
+        imageButton = (ImageButton) findViewById(R.id.ImgButton);
+        editText = (EditText) findViewById(R.id.Edittxt);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                editText.append("\n "+location.getLatitude()+""+location.getLongitude());
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent =new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+
+            }
+        };
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+
+                }, 10 );
+                return;
+            } else {
+                configureImageButton();
+            }
+        }
+
+
+
+
+
+
+
+
+
 
         mAuth = FirebaseAuth.getInstance();
         sref = FirebaseStorage.getInstance().getReference();
@@ -85,6 +160,16 @@ public class CaptionActivity extends AppCompatActivity {
         iv.setImageBitmap(bmThumbnail);
     }
 
+    private void configureImageButton() {
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                locationManager.requestLocationUpdates("gps", 5000, 100, locationListener);
+
+            }
+        });
+
+    }
 
 
     public class AsyncCaller extends AsyncTask<Void, Integer,Void >
